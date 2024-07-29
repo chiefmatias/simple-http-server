9c891bc6e16e0b0b8dbbaa0e7111a604aaa1ad20
ChiefMatias <sergio.tiago.matias@gmail.com>
2024-07-29 10:12:44 +0200

    Refactor using Switch

--- main.go
+++ main.go
@@ -6,6 +6,47 @@ import (
 	"strings"
 )
 
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection:", err)
+	}
+
+	request := string(buffer[:n])
+	parsedRequest := strings.Split(request, "\r\n")
+	requestTarget := strings.Split(parsedRequest[0], " ")
+	var responseBody string
+
+	switch {
+	case requestTarget[1] == "/":
+		responseBody := "HTTP/1.1 200 OK\r\n\r\n"
+		fmt.Println(responseBody)
+		if _, err := conn.Write([]byte(responseBody)); err != nil {
+			fmt.Println(err)
+		}
+
+	case strings.HasPrefix(requestTarget[1], "/echo/"):
+		fmt.Println("Resource found:", requestTarget[1])
+		responseText := strings.TrimPrefix(requestTarget[1], "/echo/")
+		responseBody = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(responseText)) + "\r\n\r\n" + responseText
+		fmt.Println(responseBody)
+		if _, err := conn.Write([]byte(responseBody)); err != nil {
+			fmt.Println(err)
+		}
+
+	default:
+		responseBody := "HTTP/1.1 404 Not Found\r\n\r\n"
+		fmt.Println("Resource not found:", requestTarget[1])
+		if _, err := conn.Write([]byte(responseBody)); err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println(responseBody)
+}
+
 func main() {
 	listener, err := net.Listen("tcp", ":4221")
 	if err != nil {
@@ -21,47 +62,13 @@ func main() {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
-			return
+			continue
 		}
-
 		fmt.Println("Accepted connection from:", conn.RemoteAddr())
 
-		buffer := make([]byte, 1024)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Error reading from connection:", err)
-		}
-
-		request := string(buffer[:n])
+		handleRequest(conn)
 		//fmt.Println("Received request:")
 		//fmt.Println(request)
-
-		parsedRequest := strings.Split(request, "\r\n")
-		requestTarget := strings.Split(parsedRequest[0], " ")
-		var responseBody string
-
-		if requestTarget[1] == "/" {
-			responseBody := "HTTP/1.1 200 OK\r\n\r\n"
-			fmt.Println(responseBody)
-			if _, err := conn.Write([]byte(responseBody)); err != nil {
-				fmt.Println(err)
-			}
-		} else if strings.HasPrefix(requestTarget[1], "/echo/") {
-			fmt.Println("Resource found:", requestTarget[1])
-			responseText := strings.TrimPrefix(requestTarget[1], "/echo/")
-			responseBody := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + fmt.Sprint(len(responseText)) + "\r\n\r\n" + responseText
-			fmt.Println(responseBody)
-			if _, err := conn.Write([]byte(responseBody)); err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			responseBody := "HTTP/1.1 404 Not Found\r\n\r\n"
-			fmt.Println("Resource not found:", requestTarget[1])
-			if _, err := conn.Write([]byte(responseBody)); err != nil {
-				fmt.Println(err)
-			}
-		}
-		fmt.Println(responseBody)
 
 	}
 }
